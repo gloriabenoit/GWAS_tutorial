a4d057e0d640a6e7a65bd5353a362385398c5667
Gloria Benoit <gloriabenoit921@gmail.com>
2025-01-23 15:12:33 +0100

    update GWAS

--- 3_Association_GWAS/Manhattan_plot.R
+++ 3_Association_GWAS/Manhattan_plot.R
@@ -1,15 +1,13 @@
-install.packages("qqman",repos="http://cran.cnr.berkeley.edu/",lib="~" ) # location of installation can be changed but has to correspond with the library location 
-library("qqman",lib.loc="~")  
-results_log <- read.table("logistic_results.assoc_2.logistic", head=TRUE)
+install.packages("qqman",repos="https://mirror.ibcp.fr/pub/CRAN/",lib="~") # location of installation can be changed but has to correspond with the library location 
+library("qqman",lib="~") 
+results_log <- read.table(text = sub("#", "", readLines("logistic_results_2.PHENO1.glm.logistic.hybrid")), header = TRUE)
 jpeg("Logistic_manhattan.jpeg")
-manhattan(results_log,chr="CHR",bp="BP",p="P",snp="SNP", main = "Manhattan plot: logistic")
+# manhattan(results_log,chr="CHR",bp="BP",p="P",snp="SNP", main = "Manhattan plot: logistic")
+manhattan(results_log,chr="CHROM",bp="POS",p="P",snp="ID", main = "Manhattan plot: logistic")
 dev.off()
 
-results_as <- read.table("assoc_results.assoc", head=TRUE)
-jpeg("assoc_manhattan.jpeg")
-manhattan(results_as,chr="CHR",bp="BP",p="P",snp="SNP", main = "Manhattan plot: assoc")
-dev.off()  
-
-
-
+# results_as <- read.table("assoc_results.assoc", head=TRUE)
+# jpeg("assoc_manhattan.jpeg")
+# manhattan(results_as,chr="CHR",bp="BP",p="P",snp="SNP", main = "Manhattan plot: assoc")
+# dev.off()  
 
--- 3_Association_GWAS/QQ_plot.R
+++ 3_Association_GWAS/QQ_plot.R
@@ -1,12 +1,12 @@
-install.packages("qqman",repos="http://cran.cnr.berkeley.edu/",lib="~" ) # location of installation can be changed but has to correspond with the library location 
-library("qqman",lib.loc="~") 
-results_log <- read.table("logistic_results.assoc_2.logistic", head=TRUE)
+install.packages("qqman",repos="https://mirror.ibcp.fr/pub/CRAN/",lib="~") # location of installation can be changed but has to correspond with the library location 
+library("qqman",lib="~") 
+results_log <- read.table(text = sub("#", "", readLines("logistic_results_2.PHENO1.glm.logistic.hybrid")), header = TRUE)
 jpeg("QQ-Plot_logistic.jpeg")
 qq(results_log$P, main = "Q-Q plot of GWAS p-values : log")
 dev.off()
 
-results_as <- read.table("assoc_results.assoc", head=TRUE)
-jpeg("QQ-Plot_assoc.jpeg")
-qq(results_as$P, main = "Q-Q plot of GWAS p-values : log")
-dev.off()
+# results_as <- read.table("assoc_results.assoc", head=TRUE)
+# jpeg("QQ-Plot_assoc.jpeg")
+# qq(results_as$P, main = "Q-Q plot of GWAS p-values : log")
+# dev.off()
 
