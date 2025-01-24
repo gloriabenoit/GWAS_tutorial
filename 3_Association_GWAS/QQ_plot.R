43c5ab827663f11e75e79f14bfd8f6ea10dbb404
Gloria Benoit <gloriabenoit921@gmail.com>
2025-01-24 16:58:32 +0100

    update jpeg to pdf

--- 3_Association_GWAS/QQ_plot.R
+++ 3_Association_GWAS/QQ_plot.R
@@ -1,12 +1,12 @@
 install.packages("qqman",repos="https://mirror.ibcp.fr/pub/CRAN/",lib="~") # location of installation can be changed but has to correspond with the library location 
 library("qqman",lib="~") 
 results_log <- read.table(text = sub("#", "", readLines("logistic_results_2.PHENO1.glm.logistic.hybrid")), header = TRUE)
-jpeg("QQ-Plot_logistic.jpeg")
+pdf("QQ-Plot_logistic.pdf")
 qq(results_log$P, main = "Q-Q plot of GWAS p-values : log")
 dev.off()
 
 # results_as <- read.table("assoc_results.assoc", head=TRUE)
-# jpeg("QQ-Plot_assoc.jpeg")
+# pdf("QQ-Plot_assoc.pdf")
 # qq(results_as$P, main = "Q-Q plot of GWAS p-values : log")
 # dev.off()
 
